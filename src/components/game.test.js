f75a5e7fcc1fbd03d9f9944aec8628efa44b83f9
52lions06 <mckelvey.william@yahoo.com>
2017-07-28 15:21:41 -0400

    Rendered Header, GuessList, GuessCount, and GuessSection in game.js

--- src/components/game.test.js
+++ src/components/game.test.js
@@ -1,13 +1,43 @@
 import React from 'react'
 
 import Game from './game'
+import Header from './header'
+import GuessCount from './guess-count'
+import GuessSection from './guess-section'
+import GuessList from './guess-list'
 
 import {shallow} from 'enzyme'
 
 describe('Game component', () => {
   it('smoke test for new game', () => {
     shallow (<Game />) 
-  })
+  });
+
+  it('Render the each section', () => {
+    const wrapper = shallow (<Game />);
+    const instance = wrapper.instance();
+    shallow (<Header />);
+  });
+
+  it('Rendering GuessSection', () => {
+    const wrapper = shallow (<Game />);
+    const instance = wrapper.instance();
+    shallow (<GuessSection />);
+
+  });
+
+  it('Render GuessCount', () => {
+    const wrapper = shallow (<Game />);
+    const instance = wrapper.instance();
+    shallow (<GuessCount />);
+
+  });
+
+  it('Rendering GuessList', () => {
+    const wrapper = shallow (<Game />);
+    const instance = wrapper.instance();
+    shallow (<GuessList guesses={[]}/>);
+  });
 })
 
 
